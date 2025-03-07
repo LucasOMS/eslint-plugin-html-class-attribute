743be2a886319e938a47ee0ac886321d66d4a9ec
Lucas OMS <lucas.oms@hotmail.fr>
2025-03-07 07:38:55 +0100

    feat: change way to get parser to support ESLint 9 configs

--- src/utils/get-implementation-by-parser.ts
+++ src/utils/get-implementation-by-parser.ts
@@ -5,14 +5,20 @@ export function getImplementationByParser(context: RuleContext,
                                           angularTemplateParserImplem: (context: RuleContext) => any,
                                           htmlEslintImplem: (context: RuleContext) => any): Rule.RuleListener {
 
-    const parser = context.parserPath ?? '';
+    // Check which parser is being used
+    const parser =
+        context.parserPath // ESLint 8 way to get it
+        ?? context.languageOptions?.parser?.meta?.name; // ESLint 9 way to get it
+
+    if (!parser) {
+        throw new Error('Parser is undefined');
+    }
 
     if (parser.includes('@angular-eslint')) {       // @angular-eslint/template-parser
         return angularTemplateParserImplem(context);
     } else if (parser.includes('@html-eslint')) {   // @html-eslint/parser
         return htmlEslintImplem(context);
     } else {
-        // If the parser is not supported, return an empty object to avoid any errors, no rule will be applied
-        return {};
+        throw new Error(`Unsupported parser, please use @angular-eslint/template-parser or @html-eslint/parser, current is ${parser}`);
     }
 }
